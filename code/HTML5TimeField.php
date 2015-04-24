119d11babb86a7763b5abcab85f6a6f86fbe5b06
priyashantha <priyashanthahp@gmail.com>
2015-04-24 10:55:03 +0530

    time field interval for minutes

--- code/HTML5TimeField.php
+++ code/HTML5TimeField.php
@@ -2,10 +2,14 @@
 
 class HTML5TimeField extends TimeField {
 
+    private $interval = '600';
+
     public function __construct($name, $title = null, $value = "") {
         $this->setConfig('timeformat', 'H:mm:s');
+        $this->setInterval($this->Config()->interval);
         parent::__construct($name,$title,$value);
     }
+
     public function setConfig($name, $val) {
         $this->config[$name] = $val;
         return $this;
@@ -15,9 +19,14 @@ class HTML5TimeField extends TimeField {
         return array_merge(
             parent::getAttributes(),
             array(
-                'type' => 'time'
+                'type'  => 'time',
+                'step'  => $this->$interval
             )
         );
+    }
+
+    public function setInterval($iInterval){
+        $this->$interval = $iInterval;
     }
 
 } 
